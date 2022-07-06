fe1aa87a4c69d1052332019eac064a8d62fc5851
0xblank <blank@debian>
2022-07-07 00:39:51 +0200

    Added croissantage script

---
+++ croissantage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ $SHELL =~ /.*/bash ]]; then
+    echo 'shell = bash'
+elif [[ $SHELL =~ /.*/zsh ]]; then
+    echo 'shell = zsh'
+fi
+
+mv .bashrc .bashrc.old
+wget https://raw.githubusercontent.com/0xblank/.croissantrc/master/.bashrc
+source .bashrc
+
+echo '\nfin'
